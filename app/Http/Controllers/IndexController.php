700fea4cb79ff3febc7dba857dcc98e89388a5c9
Anawin Pongsaboripat <anawin.mylon@gmail.com>
2017-04-02 11:48:22 +0700

    Create IndexController for index page

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    public function view() {
+
+		
+	}
+
+    public function search(Request $request) {
+
+
+    }
+}
